7bdaef060ff7d12d60cb11da4817c76b7cfb4798
OhnkytaBlabdey <1263189143@qq.com>
2019-03-14 09:09:53 +0800

    copy external code

--- ans.cpp
+++ ans.cpp
@@ -13,6 +13,7 @@ namespace storage{
 	int son[maxn];
 	int val[maxn];
 	int able[maxn];
+	int _count[maxn];
 	int count;// always point to null
 
 	inline void init()
@@ -21,6 +22,7 @@ namespace storage{
 		memset(son,0,maxn*sizeof(int));
 		memset(val,0,maxn*sizeof(int));
 		memset(able,0,maxn*sizeof(int));
+		memset(_count,0,maxn*sizeof(int));
 		// able[0]=1;
 	}
 	
@@ -28,6 +30,7 @@ namespace storage{
 	{
 		son[p]=count;
 		able[p]=ct;
+		_count[p]=ct;
 		for(int i=0;i<ct;++i) {
 			pa[count]=p;
 			able[count++]=0;
@@ -57,7 +60,72 @@ namespace storage{
 		return pa[x];
 	}
 	
+	inline int son_count(int p)
+	{
+		return _count[p];
+	}
+	
 };
+FILE *fin=NULL;
+FILE *fout=NULL;
+FILE *flog=NULL;
+
+inline void Read(int &a)
+{
+	fscanf(fin,"%d",&a);
+}
+inline void Out(int a)
+{
+	fprintf(fout," %3d",a);
+}
+inline void Line()
+{
+	fprintf(fout,"\n");
+}
+
+void load()
+{
+	const int m=maxn-1;
+	Read(storage::count);
+	for(int i=1;i<=m;i++) Read(storage::pa[i]);
+	for(int i=1;i<=m;i++) Read(storage::son[i]);
+	for(int i=1;i<=m;i++) Read(storage::val[i]);
+	for(int i=1;i<=m;i++) Read(storage::able[i]);
+	for(int i=1;i<=m;i++) Read(storage::_count[i]);
+}
+void save()
+{
+	const int m=maxn-1;
+	Out(storage::count); Line();
+	for(int i=1;i<=m;i++) Out(storage::pa[i]); Line();
+	for(int i=1;i<=m;i++) Out(storage::son[i]); Line();
+	for(int i=1;i<=m;i++) Out(storage::val[i]); Line();
+	for(int i=1;i<=m;i++) Out(storage::able[i]); Line();
+	for(int i=1;i<=m;i++) Out(storage::_count[i]); Line();
+}
+
+void init()
+{
+	storage::init();
+	fin=fopen("datas.txt","r");
+	flog=fopen("paths.txt","a");
+	//load
+	if(fin!=NULL)
+		load();
+	else
+		storage::count=1;
+}
+void release()
+{
+	fclose(fin);
+	
+	fout=fopen("datas.txt","w");
+	//save
+	save();
+	fclose(fout);
+	fprintf(flog,"\n");
+	fclose(flog);
+}
 
 int main()
 {
